c18acc6a2544f4a3abaabe27b5125270e1b895b6
NIVAS BODAPATI <nivasbodapati.abc@gmail.com>
2023-03-10 14:29:48 +0530

    added automatic email using emialjs

--- src/components/ContactMe/contact.jsx
+++ src/components/ContactMe/contact.jsx
@@ -1,8 +1,10 @@
 import classes from './contact.module.css';
-import emailjs from '@emailjs/browser';
-import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import { useRef, useState } from 'react';
 
 const Contact = () => {
+    const [sent, setSent] = useState(false);
+    const [error, setError] = useState(false);
     const form2 = useRef();
     const clearFields = () => {
         document.getElementById('name').value = "";
@@ -12,14 +14,30 @@ const Contact = () => {
     }
     const sendEmail = (e) => {
         e.preventDefault();
-        emailjs.sendForm('service_portfolio', 'template_ljmxpt4', form2.current, 'pLKp295GbvzAIxp6a')
+        emailjs.sendForm('service_portfolio', 'template_ljmxpt4', e.target, 'pLKp295GbvzAIxp6a')
         .then((result) => {
             console.log(result.text);
+            setSent(true)
+            setInterval(()=>{
+                setSent(false)
+            }, 3000);
+            clearFields();
         }, (error) => {
             console.log(error.text);
+            setError(true)
+            setInterval(()=>{
+                setError(false)
+            }, 3000);
+            
         });
     }
     return <div className={classes.contact}>
+        <div className={`${classes.sent} ${sent? classes.show:classes.hide}`}>
+            <p>You mail has been recieved. Thanks for contacting me.</p>
+        </div>
+        <div className={`${classes.error} ${error? classes.show:classes.hide}`}>
+            <p>Email not sent. Please try sending email to nivasbodapati.abc@gmail.com</p>
+        </div>
         <div className={classes.h3}>
             <h3>Contact Me</h3>
             <span>Please don't hesitate to use the form, to get in touch with me.</span>
@@ -27,11 +45,11 @@ const Contact = () => {
         <form ref={form2} className={classes.form} onSubmit={sendEmail}>
             <div className={classes['name-email']}>
                 <input type="text" name='name' id='name' placeholder='Enter Your Name' autoComplete='off'/>
-                <input type="email" name="email" id="email" placeholder='Enter Your Email' autoComplete='off'/>
+                <input type="email" name="email" id="email" placeholder='Enter Your Email' autoComplete='off' />
             </div>
-            <input type="text" name="subject" id="subject" placeholder='Subject' className={classes.subject} autoComplete='off'/>
-            <textarea name="message" id="message" cols="30" rows="5" placeholder="Message" autoComplete='off'></textarea>
-            <button type="submit" onClick={clearFields}>Send A Mail!</button>
+            <input type="text" name="subject" id="subject" placeholder='Subject' className={classes.subject} autoComplete='off' />
+            <textarea name="message" id="message" cols="30" rows="5" placeholder="Message" autoComplete='off' ></textarea>
+            <button type="submit">Send A Mail!</button>
         </form>
     </div>
 };
